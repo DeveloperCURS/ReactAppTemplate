7f67c9bdc7280000be2f07631815308d107a2b09
OrazovTairlan <orazovtairlan@gmail.com>
2022-03-09 23:12:18 +0600

    added render props to AppBar

--- src/AppBar.tsx
+++ src/AppBar.tsx
@@ -1,17 +1,21 @@
-import React, {FC} from 'react';
+import React, {FC, ReactElement, ReactNode} from 'react';
 import {AppBarItem, directions} from "./types/types";
 import App from "./App";
+import classNames from "classnames";
 
 interface IProps {
-    direction: directions;
-    AppBarItem: AppBarItem;
+    direction?: directions;
+    render: any;
+    data: Array<AppBarItem>;
 }
 
-const AppBar: FC<IProps> = ({direction = "vertical"}) => {
-    return (
-        <div>
-        </div>
-    );
+const AppBar: FC<IProps> = ({direction = "horizontal", data, render}) => {
+    /*let result = data.map((item) => {
+        return <ItemComponent>
+            asdasd
+        </ItemComponent>
+    });*/
+    return <div>{render("something")}</div>
 };
 
 export default AppBar;
