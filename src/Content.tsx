79f6ede7e3c9c7e2c07048bc687c3f9ac9cbdda7
OrazovTairlan <orazovtairlan@gmail.com>
2022-03-10 11:17:36 +0600

    Added content component

--- src/Content.tsx
+++ src/Content.tsx
@@ -1,17 +1,18 @@
-import React, {FC, ReactElement} from 'react';
+import React, {FC, forwardRef, memo, ReactElement} from 'react';
 import classNames from "classnames";
 
 interface IProps {
     ContentComponent: React.ElementType;
     cssNames: Array<String>;
+    children: React.ReactNode;
 }
 
-const Content: FC<IProps> = ({ContentComponent, cssNames}) => {
+const Content: FC<IProps> = (props, ref) => {
     return (
-        <div className={classNames(cssNames)}>
-            <ContentComponent/>
+        <div className={classNames(props.cssNames)}>
+            {props.children}
         </div>
     );
 };
 
-export default Content;
+export default memo(Content);
